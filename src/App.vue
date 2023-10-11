386c3cd6053065f2ebfe999fcc4fcae21fe71037
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2023-10-11 13:51:48 +0200

    Delete commented lines

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-// import HelloWorld from './components/HelloWorld.vue';
-// import TheWelcome from './components/TheWelcome.vue';
 import PaginatedAutocomplete from './components/PaginatedAutocomplete.vue';
 
 const records = ref([]);
@@ -77,14 +75,6 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header> -->
-
   <main>
     <PaginatedAutocomplete
       v-model="selectedRecord"
