8e873ea5b738fca1db733920285560ae57179992
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2023-10-11 10:23:28 +0200

    Create PaginatedAutocomplete, adjust the home page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
+import { ref } from 'vue';
+
+// import HelloWorld from './components/HelloWorld.vue';
+// import TheWelcome from './components/TheWelcome.vue';
+import PaginatedAutocomplete from './components/PaginatedAutocomplete.vue';
+
+const records = ref([]);
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
-  </header>
+  </header> -->
 
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <PaginatedAutocomplete :items="records" />
   </main>
 </template>
 
---
+++ src/components/PaginatedAutocomplete.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-autocomplete :items="items" :menu-props="{ maxWidth: 300 }" class="autocomplete">
+    <!--  -->
+  </v-autocomplete>
+</template>
+
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.autocomplete {
+  width: 300px;
+}
+</style>
