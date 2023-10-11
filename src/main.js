7d0361bd0a2724c4e571c1a65f355210865046df
Mihailo Obradović <mihailo.obradovic.95@gmail.com>
2023-10-11 10:08:57 +0200

    Add Vuetify and optional dependencies

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { VAutocomplete } from 'vuetify/components';
+import { Intersect } from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components: { VAutocomplete },
+  directives: { Intersect }
+});
+
+createApp(App).use(vuetify).mount('#app');
